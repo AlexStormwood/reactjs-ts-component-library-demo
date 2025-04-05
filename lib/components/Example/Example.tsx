79d90f5d8b6ee335aac6e91361dbeb74adb2a60f
Alex <alex.holder@bigfootds.com>
2025-04-05 21:46:31 +1100

    fix: improve component content

--- lib/components/Example/Example.tsx
+++ lib/components/Example/Example.tsx
@@ -3,7 +3,8 @@ import "./Example.css";
 export default function ExampleComponent(){
 	return (
 		<section className="exampleComponent">
-			Hello, world!
+			<h1>Hello, world!</h1>
+			<p>This is a demo component, it just renders some text.</p>
 		</section>
 	)
 }
